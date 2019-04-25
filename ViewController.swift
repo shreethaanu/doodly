4e7f54ea27a3f16dd07f1b338e2d54ba1e73761d
shreethaanu raveendran <shree96thaanu@gmail.com>
2019-04-25 11:06:10 +0530

    Fixed the imageview stretch Issue

---
+++ ViewController.swift
@@ -0,0 +1,168 @@
+//
+//  ViewController.swift
+//  Doodly
+//
+//  Created by ShreeThaanu RK on 23/04/19.
+//  Copyright © 2019 ShreeThaanu RK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var tempImageView: UIImageView!
+    
+    @IBOutlet weak var colourPallet: UIView!
+    
+    var lastPoint = CGPoint.zero
+    var red: CGFloat = 0.0
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+    var brushWidth: CGFloat = 10.0
+    var opacity: CGFloat = 1.0
+    var swiped = false
+    
+    let colors: [(CGFloat, CGFloat, CGFloat)] = [
+        (0, 0, 0),
+        (105.0 / 255.0, 105.0 / 255.0, 105.0 / 255.0),
+        (1.0, 0, 0),
+        (0, 0, 1.0),
+        (51.0 / 255.0, 204.0 / 255.0, 1.0),
+        (102.0 / 255.0, 204.0 / 255.0, 0),
+        (102.0 / 255.0, 1.0, 0),
+        (160.0 / 255.0, 82.0 / 255.0, 45.0 / 255.0),
+        (1.0, 102.0 / 255.0, 0),
+        (1.0, 1.0, 0),
+        (1.0, 1.0, 1.0),
+        ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       colourPallet.isHidden = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func showColourPallet(_ sender: Any) {
+   colourPallet.isHidden = false
+    
+    }
+    
+    @IBAction func colourPalletDoneAction(_ sender: Any) {
+        colourPallet.isHidden = true
+    }
+    
+    @IBAction func Clear(_ sender: Any) {
+    mainImageView.image = nil
+        tempImageView.image = nil
+    }
+   
+    @IBAction func shareIt(_ sender: Any) {
+        UIGraphicsBeginImageContext(mainImageView.bounds.size)
+        mainImageView.image?.draw(in: CGRect(x: 0, y: 0,
+                                             width: mainImageView.frame.size.width, height: mainImageView.frame.size.height))
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        let activity = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+        present(activity, animated: true, completion: nil)
+    }
+    
+    @IBAction func pencilPressed(_ sender: Any) {
+        
+        var index = (sender as AnyObject).tag ?? 0
+                if index < 0 || index >= colors.count {
+                    index = 0
+                }
+        
+                (red, green, blue) = colors[index]
+        
+                if index == colors.count - 1 {
+                    opacity = 1.0
+                }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        colourPallet.isHidden = true
+        swiped = false
+        if let touch = touches.first {
+            lastPoint = touch.location(in: self.view)
+        }
+    }
+    
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+        
+        UIGraphicsBeginImageContext(view.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        tempImageView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+        
+        context!.move(to: CGPoint(x: fromPoint.x, y: fromPoint.y))
+        context!.addLine(to: CGPoint(x: toPoint.x, y: toPoint.y))
+        
+        context!.setLineCap(CGLineCap.round)
+        context!.setLineWidth(brushWidth)
+        context!.setStrokeColor(red: red, green: green, blue: blue, alpha: 1.0)
+        context!.setBlendMode(CGBlendMode.normal)
+        context!.strokePath()
+        
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempImageView.alpha = opacity
+        UIGraphicsEndImageContext()     }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+      colourPallet.isHidden = true
+      swiped = true
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: view)
+            drawLineFrom(fromPoint: lastPoint, toPoint: currentPoint)
+            lastPoint = currentPoint
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if !swiped {
+            
+           // drawLineFrom(fromPoint: lastPoint, toPoint: lastPoint)
+        }
+       
+//        UIGraphicsBeginImageContext(mainImageView.frame.size)
+//        mainImageView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: .normal, alpha: 1.0)
+//        tempImageView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: .normal, alpha: opacity)
+//        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+//     //   UIGraphicsEndImageContext()
+//
+      
+    }
+
+}
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//
+//        let settingsViewController = segue.destination as! SettingsViewController
+//        settingsViewController.delegate = self
+//        settingsViewController.brush = brushWidth
+//        settingsViewController.opacity = opacity
+//        settingsViewController.red = red
+//        settingsViewController.green = green
+//        settingsViewController.blue = blue
+//    }
+    
+
+
+//extension ViewController: SettingsViewControllerDelegate {
+//    func settingsViewControllerFinished(settingsViewController: SettingsViewController) {
+//        self.brushWidth = settingsViewController.brush
+//        self.opacity = settingsViewController.opacity
+//        self.red = settingsViewController.red
+//        self.green = settingsViewController.green
+//        self.blue = settingsViewController.blue
+//    }
+
